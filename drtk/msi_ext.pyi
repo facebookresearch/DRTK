7ec218dc175be9101ec4b7de649643c01ff4a310
Stanislav Pidhorskyi <stpidhorskyi@meta.com>
2024-09-26 07:44:20 -0700

    Fix typo. We import `from torch import Tensor` but then were using `th.Tensor` instead of `Tensor`
    Differential Revision: D63440224
    
    fbshipit-source-id: 0a3647fd710f2d980b22f1954e439fa27a1b4864

--- drtk/msi_ext.pyi
+++ drtk/msi_ext.pyi
@@ -6,11 +6,11 @@
 from torch import Tensor
 
 def msi(
-    ray_o: th.Tensor,
-    ray_d: th.Tensor,
-    texture: th.Tensor,
+    ray_o: Tensor,
+    ray_d: Tensor,
+    texture: Tensor,
     sub_step_count: int,
     min_inv_r: float,
     max_inv_r: float,
     stop_thresh: float,
-) -> th.Tensor: ...
+) -> Tensor: ...
