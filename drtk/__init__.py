e2335d06b989e8f602c932a5a9ef6babf1d7fd84
Stanislav Pidhorskyi <stpidhorskyi@meta.com>
2024-09-26 07:44:20 -0700

    Make `rasterize_with_depth` and `transform_with_v_cam` accessable from the root drtk module
    Summary:
    Make `rasterize_with_depth` and `transform_with_v_cam`  accessible from the root drtk module as:
    
    ```
    from drtk import rasterize_with_depth, transform_with_v_cam
    ```
    
    instead of
    
    ```
    from drtk.rasterize import rasterize_with_depth
    from drtk.transform import transform_with_v_cam
    ```
    
    This has greater importance for DGX and GitHub rather than prod, since because of how buck is setup, it still won't work.
    
    Reviewed By: una-dinosauria
    
    Differential Revision: D63440390
    
    fbshipit-source-id: 7f6c8c16fb8ef5aa10bef3491fd59727f1000d0b

--- drtk/__init__.py
+++ drtk/__init__.py
@@ -9,8 +9,8 @@ from .grid_scatter import grid_scatter, grid_scatter_ref  # noqa
 from .interpolate import interpolate, interpolate_ref  # noqa
 from .mipmap_grid_sample import mipmap_grid_sample, mipmap_grid_sample_ref  # noqa
 from .msi import msi  # noqa
-from .rasterize import rasterize  # noqa
+from .rasterize import rasterize, rasterize_with_depth  # noqa
 from .render import render, render_ref  # noqa
-from .transform import transform  # noqa
+from .transform import transform, transform_with_v_cam  # noqa
 
 __version__ = "0.1.0"  # noqa
