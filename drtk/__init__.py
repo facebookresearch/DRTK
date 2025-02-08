e95d43c6375d5036461ee4aecac4b7e2462b77fc
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-02-07 17:14:59 -0800

    Add missing Pyre mode headers] [batch:9/48] [shard:2/N]
    Reviewed By: ViniGupta08
    
    Differential Revision: D69296397
    
    fbshipit-source-id: f7b014bcaa0c4fecee13acf1aa4b75b3fe0ebb5e

--- drtk/__init__.py
+++ drtk/__init__.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 from . import utils  # noqa  # noqa
 from .edge_grad_estimator import edge_grad_estimator, edge_grad_estimator_ref  # noqa
 from .grid_scatter import grid_scatter, grid_scatter_ref  # noqa
